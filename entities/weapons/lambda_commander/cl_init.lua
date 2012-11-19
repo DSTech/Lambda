5bae801f8609a075882b2730a277581ed0d33a0d
Dessi <Dessix@D6Gaming.net>
2012-11-18 23:40:44 -0500

    Modified the commander to use the net library, and made all selection clientside. View the SWEP:Order function to see the format that should be expected.
    Signed-off-by: Dessi <Dessix@D6Gaming.net>

--- entities/weapons/lambda_commander/cl_init.lua
+++ entities/weapons/lambda_commander/cl_init.lua
@@ -6,6 +6,147 @@ SWEP.SlotPos = 4
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = true
 
+SWEP.LeftDown = false
+SWEP.RightDown = false
+SWEP.ReloadDown = false
+
+function SWEP:Think()
+	local leftdown,rightdown,reloaddown = self.Owner:KeyDown(IN_ATTACK),self.Owner:KeyDown(IN_ATTACK2),self.Owner:KeyDown(IN_RELOAD)
+	if(leftdown and (not self.LeftDown))then
+		self.LeftDown=true;
+		self:StartLeftClick();
+	elseif((not leftdown) and self.LeftDown)then
+		self.LeftDown=false;
+		self:EndLeftClick();
+	end
+	if(rightdown and (not self.RightDown))then
+		self.RightDown=true;
+		self:StartRightClick();
+	elseif((not rightdown) and self.RightDown)then
+		self.RightDown=false;
+		self:EndRightClick();
+	end
+	if(reloaddown and (not self.ReloadDown))then
+		self.ReloadDown=true;
+		self:StartReload();
+	elseif((not reloaddown) and self.ReloadDown)then
+		self.ReloadDown=false;
+		self:EndReload();
+	end
+end
+
+function SWEP:PrimaryAttack()
+	self:Think()
+end
+
+function SWEP:SecondaryAttack()
+	self:Think()
+end
+
+function SWEP:Reload()
+	self:Think()
+end
+--------------------------------------------------------
+
+function SWEP:StartLeftClick()
+	local tr = self.Owner:GetEyeTrace() if(tr.Hit)then self:StartSelect(tr.HitPos) end
+end
+function SWEP:EndLeftClick()
+	local tr = self.Owner:GetEyeTrace() if(tr.Hit)then self:EndSelect(tr.HitPos, "radial") else self:CancelSelect() end
+end
+
+function SWEP:StartRightClick()
+	local tr = self.Owner:GetEyeTrace()
+	if(tr.Hit)then
+		local cursel = {}--New selection storage
+		for k,v in pairs(self.selection)do
+			cursel[k]=v--Copy over selection
+		end
+		self:Order({type="move",pos=tr.HitPos}, cursel)--Send new copy of selection alongside the order
+	end
+end
+function SWEP:EndRightClick()
+end
+
+function SWEP:StartReload()
+	self:ClearSelection(true)
+end
+function SWEP:EndReload()
+end
+
+function SWEP:ShapeSelect(startpos, endpos, shape, mode)
+	mode = mode or 1;
+	shape = shape or "radial";
+	local entlist
+	if(shape=="radial")then
+		entlist = self:SphereSelect(startpos, endpos)
+	else
+		entlist = {}
+	end
+
+	self.selection = self.selection or {};
+	if(mode==1)then
+		self:ClearSelection(false)
+		for k,v in pairs(entlist) do
+			self.selection[v]=v;
+		end
+	elseif(mode==2)then
+		for k,v in pairs(entlist) do
+			self.selection[v]=v;
+		end
+	elseif(mode==3)then
+		for k,v in pairs(entlist) do
+			self.selection[v]=nil;
+		end
+	end
+	return self.selection
+end
+
+function SWEP:IsCommandable(ent)
+	return(ent.lambda==true)
+end
+
+function SWEP:IsOwnedEnt(ent)
+	local ownerTeam = dTeams:getTeam(self.Owner)
+	return (ownerTeam and ownerTeam == dTeams:getTeam(ent))
+end
+
+function SWEP:SphereSelect(startpos, endpos)
+	local entlist = ents.FindInSphere(startpos, math.max(startpos:Distance(endpos), 1))
+	local outlist = {}
+	for k,v in pairs(entlist)do
+		if(self:IsCommandable(v) and self:IsOwnedEnt(v))then
+			table.insert(outlist, v)
+		end
+	end
+	return outlist
+end
+
+SWEP.cursel = nil
+function SWEP:StartSelect(pos)
+	if self.cursel then
+		return
+	end
+	self.cursel = {startpos = pos}
+end
+
+function SWEP:EndSelect(pos,shape)
+	if not self.cursel then
+		return
+	end
+	self.cursel.shape = shape or "radial"
+	self.cursel.endpos = pos
+	self.cursel.mode = ((self.Owner:KeyDown(IN_SPEED) and 2) or (self.Owner:KeyDown(IN_WALK) and 3)) or 1;
+	local entlist = self:ShapeSelect(self.cursel.startpos, self.cursel.endpos, self.cursel.shape, self.cursel.mode)
+	self.cursel = nil
+	self:SelectionChanged()
+end
+
+function SWEP:CancelSelect()
+	self.cursel = nil
+end
+
+
 SWEP.LaserMat = Material("cable/cable2")
 SWEP.LaserColor = Color(0,0,255,255)
 
@@ -76,5 +217,22 @@ function SWEP:DrawRadialSelection(center, edge)
 	cam.End3D()
 end
 
-function SWEP:Order(command)
+function SWEP:Order(command, units)
+	if not(command and units and #units > 0)then
+		return false
+	end
+	local cmdstring = von.serialize(command)
+	local unitarr = {}
+	for k,v in pairs(units)do
+		if(IsValid(v))then
+			table.insert(unitarr, v)
+		end
+	end
+	net.Start("Lambda Order")
+	net.WriteString(cmdstring)
+	for k,v in pairs(unitarr)do
+		net.WriteEntity(v)
+	end
+	net.SendToServer()
+	return true
 end
--- entities/weapons/lambda_commander/init.lua
+++ entities/weapons/lambda_commander/init.lua
@@ -4,25 +4,18 @@ include("shared.lua")
 SWEP.Weight = 5
 SWEP.AutoSwitchTo = false
 SWEP.AutoSwitchFrom = false
-SWEP.selection = {}
 
-function SWEP:SelectionChanged()
+function SWEP:PrimaryAttack()
+	self:Think()
 end
 
-function SWEP:ClearSelection(notify)
-	self.selection = {}
+function SWEP:SecondaryAttack()
+	self:Think()
 end
 
-function SWEP:AssignOrder(ent, command)
-	if(ent.Order)then ent:Order(command) end
+function SWEP:Reload()
+	self:Think()
 end
 
-function SWEP:DistributeOrder(entlist, command)
-	for k,v in pairs(entlist)do
-		self:AssignOrder(v, command)
-	end
-end
-
-function SWEP:Order(command)
-	self:DistributeOrder(self.selection, command)
+function SWEP:Think()
 end
