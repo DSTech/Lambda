c187b07c2d887a21fc5e12f7399da47b7d0b98da
Dessi <Dessix@D6Gaming.net>
2012-11-19 17:35:59 -0500

    Added functions to distribute commands based on the unit list provided, and prevents units from receiving commands from other teams.
    Signed-off-by: Dessi <Dessix@D6Gaming.net>

--- gamemode/lambda_commands.lua
+++ gamemode/lambda_commands.lua
@@ -2,10 +2,32 @@ util.AddNetworkString("Lambda Order")
 net.Receive("Lambda Order", function(len, plr)
 	local order = von.deserialize(net.ReadString())
 	local entc = net.ReadUInt(16)
-	local entlst = {}
+	local entities = {}
 	for i = 1, entc do
-		table.insert(entlst, net.ReadEntity())
+		table.insert(entities, net.ReadEntity())
 	end
-	PrintTable(order)
-	plr:ChatPrint("Order received with "..(#entlst).." units as recipients!")
+	local err, msg = pcall(multiAssignOrder, plr, order, entities)
+	if(err == false)then
+		ErrorNoHalt(msg)
+	end
 end)
+
+function multiAssignOrder(commander, order, entities)
+	print("Order:")
+	PrintTable(order)
+	commander:ChatPrint("Order received with "..(#entities).." units as recipients!")
+	for k,v in pairs(entities)do
+		assignOrder(commander, order, v)
+	end
+end
+
+function assignOrder(commander, order, entity)
+	if((not IsValid(commander))
+			or (not IsValid(entity))
+			or (not order)
+			or dTeams:getTeam(commander) ~= dTeams:getTeam(entity)
+			or type(entity.Order)~="function")then
+		return false
+	end
+	entity:Order(order)
+end
